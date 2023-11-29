f5927d314e89efd1423ecc5335b2fba939f8d161
locodafux <locodafux@gmail.com>
2023-11-29 15:50:42 +0800

    added validation

--- bin2dec/script.js
+++ bin2dec/script.js
@@ -1,8 +1,25 @@
 function binaryToDecimal() {
   var binary = document.getElementById("binary").value;
   var output = document.getElementById("output");
-
+  var alert = document.getElementById("alert");
+  alert.style.display = "none";
   const arrayBinary = binary.split("");
+
+  //validation
+  for (var i = 0; i < arrayBinary.length; i++) {
+    if (isNaN(arrayBinary[i])) {
+      alert.innerHTML = "Please enter only 1's or 0's";
+      alert.style.display = "block";
+    }
+    if (arrayBinary[i] !== "0" && arrayBinary[i] !== "1") {
+      alert.innerHTML = "Please enter only 1's or 0's";
+      alert.style.display = "block";
+
+      return;
+    }
+  }
+
+  //Removing 0s before 1s;
   var spliceBinary = [];
   for (var i = 0; i < binary.length; i++) {
     if (arrayBinary[i] === "1") {
@@ -11,6 +28,12 @@ function binaryToDecimal() {
     }
   }
 
+  if (spliceBinary.length > 8) {
+    alert.innerHTML = "Please input a maximum of eight binary numbers.";
+    alert.style.display = "block";
+    binary.value = "";
+    return;
+  }
   var num = 2;
   for (var i = 0; i < spliceBinary.length; i++) {
     if (i === 1) {
@@ -30,5 +53,6 @@ function binaryToDecimal() {
       }
     }
   }
-  output.innerHTML = num;
+  //output
+  output.innerHTML = "Decimal base 10 : " + num;
 }
