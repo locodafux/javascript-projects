26c4e64fee8c10658648b6ec305a173b67087373
locodafux <locodafux@gmail.com>
2023-12-05 18:56:52 +0800

    add operations

--- calculator/script.js
+++ calculator/script.js
@@ -13,6 +13,7 @@ var AC = document.getElementById("AC");
 var display = document.getElementById("display");
 var display = document.getElementById("display");
 var plus = document.getElementById("+");
+var times = document.getElementById("x");
 var operators = ["+", "-", "/", "*"];
 console.log("wow");
 seven.addEventListener("click", sevenNum);
@@ -31,31 +32,60 @@ zero.addEventListener("click", zeroNum);
 C.addEventListener("click", clear);
 AC.addEventListener("click", clearAll);
 plus.addEventListener("click", addNum);
+times.addEventListener("click", multiplyNum);
 
 function addNum() {
   display.innerHTML += "+";
+  total();
+}
+function multiplyNum() {
+  display.innerHTML += "x";
   total();
 }
 function total() {
   var total = display.textContent.split("");
   for (var i = 0; i < total.length; i++) {
     if (total[i] === "+") {
-      total = total.join("");
-      total = total.split("+");
-
-      var left = Number(total[0]);
-      var right = Number(total[1]);
-
-      if (right === "") {
-        right = 0;
-        display.innerHTML += left + "+";
-        return;
-      }
-      total = left + right;
-      display.innerHTML = total.toString() + "+";
+      sum(total);
+    }
+    if (total[i] === "x") {
+      product(total);
     }
   }
+  total = display.textContent.split("");
 }
+function sum(total) {
+  total = total.join("");
+  total = total.split("+");
+
+  var left = Number(total[0]);
+  var right = Number(total[1]);
+
+  if (right === "") {
+    right = 0;
+    display.innerHTML = left + "+";
+    return;
+  }
+  total = left + right;
+  display.innerHTML = total.toString() + "+";
+}
+
+function product(total) {
+  total = total.join("");
+  total = total.split("x");
+
+  var left = Number(total[0]);
+  var right = Number(total[1]);
+
+  if (right === 0) {
+    right = 1;
+    display.innerHTML = left + "x";
+    return;
+  }
+  total = left * right;
+  display.innerHTML = total.toString() + "x";
+}
+
 function sevenNum() {
   display.innerHTML += "7";
 }
